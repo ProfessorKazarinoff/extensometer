feefdf3a37bbef02eac75b9c639b5b519ec9e953
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2018-01-22 07:50:46 -0800

    added psuedo code

---
+++ psuedo_code.py
@@ -0,0 +1,24 @@
+# shebang
+
+# imports
+# opencv
+# imutils
+# numpy
+# pandas
+# matplotlib
+# GUI framework (flask like?)
+
+## Import video stream
+
+## parse out one frame
+
+## find red dots on each frame, find center of red dots
+
+## measure red dot pixel distance, convert to inches
+
+## build array of time vs. distance
+
+## plot time vs. distance
+
+## output .cvs of time and distance
+
