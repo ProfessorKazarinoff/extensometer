898c4ef18d483597d581ff1239bfa65f5c187410
Kazarinoff <peter.kazarinoff@ad.pcc.edu>
2018-04-05 15:25:26 -0700

    made run.py call video_dot_distance.py

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import red_dot_measure
+#import red_dot_measure
 
 def main():
+    import video_dot_distance
     # import video or open webcam
     # parse out an individual frame
     # for frame in frams:
-    video_dot_distance.main()
+
     # build numpy array of distances
 
 if __name__ == '__main__':
